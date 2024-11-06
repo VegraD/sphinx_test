dc3029d12f173256e2526d60490914907311ee6d
Vegard Johansen <vejo@stud.ntnu.no>
2024-11-06 19:44:23 +0100

    test3

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,11 @@ author = 'vejo'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
